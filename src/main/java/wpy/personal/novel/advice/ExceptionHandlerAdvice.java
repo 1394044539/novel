68c1659728ce9ff81b78599b804a6f350dc8fd47
1394044539 <1394044539@qq.com>
2021-09-25 22:00:00 +0800

    工具类失败异常捕捉

--- src/main/java/wpy/personal/novel/advice/ExceptionHandlerAdvice.java
+++ src/main/java/wpy/personal/novel/advice/ExceptionHandlerAdvice.java
@@ -3,8 +3,10 @@ package wpy.personal.novel.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import wpy.personal.novel.base.enums.ResponseCode;
 import wpy.personal.novel.base.exception.BusinessException;
 import wpy.personal.novel.base.exception.RequestException;
+import wpy.personal.novel.base.exception.UtilsException;
 import wpy.personal.novel.base.result.ResponseResult;
 
 @Slf4j
@@ -12,7 +14,7 @@ import wpy.personal.novel.base.result.ResponseResult;
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(value = RuntimeException.class)
-    public ResponseResult requestExceptionHander(RuntimeException exception){
+    public ResponseResult requestExceptionHandler(RuntimeException exception){
         if(exception!=null){
             log.error(exception.getMessage(),exception);
         }
@@ -20,7 +22,7 @@ public class ExceptionHandlerAdvice {
     }
 
     @ExceptionHandler(value = RequestException.class)
-    public ResponseResult requestExceptionHander(RequestException exception){
+    public ResponseResult requestExceptionHandler(RequestException exception){
         if(exception.getE()!=null){
             log.error(exception.getMessage());
         }
@@ -28,10 +30,18 @@ public class ExceptionHandlerAdvice {
     }
 
     @ExceptionHandler(value = BusinessException.class)
-    public ResponseResult businessExceptionHander(BusinessException exception){
+    public ResponseResult businessExceptionHandler(BusinessException exception){
         if(exception.getE()!=null){
             log.error(exception.getMessage());
         }
         return ResponseResult.error(exception.getStatus(),exception.getMsg());
+    }
+
+    @ExceptionHandler(value = UtilsException.class)
+    public ResponseResult UtilsExceptionHandler(UtilsException exception){
+        if(exception.getE()!=null){
+            log.error(exception.getMessage());
+        }
+        return ResponseResult.error(ResponseCode.FAIL.getCode(),exception.getMsg());
     }
 }
